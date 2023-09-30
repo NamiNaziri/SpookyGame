a6a4ed76c4abcfeba3b788b9211a9197f44a4266
Nami <naminaziri1@gmail.com>
2023-09-30 19:12:26 +0300

    Initial Setup

---
+++ Source/SpookyGame/SpookyGame.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpookyGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpookyGame, "SpookyGame" );
+
+DEFINE_LOG_CATEGORY(LogSpookyGame)
+ 
